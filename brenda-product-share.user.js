9f9db35f8b02fe52510408d2f09304ade5e8c36c
Lelouch <41214918+Xalavar@users.noreply.github.com>
2024-01-18 18:40:01 -0800

    Version 1.3
    * Added support for variations. They are now submitted with the API request as comments. They include the variant type (e.g., color, size).
    * Added a pop-up prompt for users to add their API token, which is now saved to Tampermonkey's internal storage. This way, users don't have to re-add their token every time the script gets updated.
    * Added some new icons to the button and removed redundant styling.
    * Data for posted products over 90 days old is removed from storage.
    * Code improvements.

--- brenda-product-share.user.js
+++ brenda-product-share.user.js
@@ -1,21 +1,15 @@
 // ==UserScript==
 // @name         Vine Discord Poster
 // @namespace    http://tampermonkey.net/
-// @version      1.2
+// @version      1.3
 // @description  A tool to make posting to Discord easier
-// @author       Discord: lelouch_di_britannia
+// @author       lelouch_di_britannia (Discord)
 // @match        https://www.amazon.com/vine/vine-items
-// @match        https://www.amazon.com/vine/vine-items?queue=potluck*
-// @match        https://www.amazon.com/vine/vine-items?queue=last_chance*
-// @match        https://www.amazon.com/vine/vine-items?queue=encore*
+// @match        https://www.amazon.com/vine/vine-items?queue=*
 // @match        https://www.amazon.co.uk/vine/vine-items
-// @match        https://www.amazon.co.uk/vine/vine-items?queue=potluck*
-// @match        https://www.amazon.co.uk/vine/vine-items?queue=last_chance*
-// @match        https://www.amazon.co.uk/vine/vine-items?queue=encore*
+// @match        https://www.amazon.co.uk/vine/vine-items?queue=*
 // @match        https://www.amazon.ca/vine/vine-items
-// @match        https://www.amazon.ca/vine/vine-items?queue=potluck*
-// @match        https://www.amazon.ca/vine/vine-items?queue=last_chance*
-// @match        https://www.amazon.ca/vine/vine-items?queue=encore*
+// @match        https://www.amazon.ca/vine/vine-items?queue=*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=amazon.com
 // @updateURL    https://raw.githubusercontent.com/xalavar/vine-product-sharing/main/brenda-product-share.user.js
 // @downloadURL  https://raw.githubusercontent.com/xalavar/vine-product-sharing/main/brenda-product-share.user.js
@@ -34,28 +28,127 @@ NOTES:
 (function() {
     'use strict';
 
-    const API_TOKEN = ""; // <<-- Replace with your own user token obtained in the Discord server ("/api-token")
+    (GM_getValue("config")) ? GM_getValue("config") : GM_setValue("config", {}); // initialize the list of items that were posted to Discord
 
-    /* ^ EVERYTHING NORMAL USERS NEED IS ABOVE HERE ^ */
+    var API_TOKEN = GM_getValue("apiToken");
+
+    function purgeOldItems() {
+        const items = GM_getValue("config");
+        const date = new Date().getTime();
+
+        for (const obj in items) {
+            ((date - items[obj].date) >= 7776000000) ? delete items[obj] : null;
+        }
+        GM_setValue("config", items);
+
+    }
+
+    purgeOldItems();
+
+    function addGlobalStyle(css) {
+        var head, style;
+        head = document.getElementsByTagName('head')[0];
+        if (!head) {
+            return;
+        }
+        style = document.createElement('style');
+        style.type = 'text/css';
+        style.innerHTML = css;
+        head.appendChild(style);
+    }
+
+    addGlobalStyle(`.a-button-discord-icon { background-image: url(https://m.media-amazon.com/images/S/sash/ZNt8quAxIfEMMky.png); content: ""; padding: 10px 10px 10px 10px; background-size: 512px 512px; background-repeat: no-repeat; margin-right: 5px; vertical-align: middle; }`)
 
     const urlData = window.location.href.match(/(amazon..+)\/vine\/vine-items(?:\?queue=)?(encore|last_chance|potluck)?.*$/); // Country and queue type are extrapolated from this
 
-    // The individual icons for the Share button
+    // Icons for the Share button
     const btn_discordSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -15 130 130" style="height: 25px;width: 26px;margin-right: 4px;">
         <path d="M107.7,8.07A105.15,105.15,0,0,0,81.47,0a72.06,72.06,0,0,0-3.36,6.83A97.68,97.68,0,0,0,49,6.83,72.37,72.37,0,0,0,45.64,0,105.89,105.89,0,0,0,19.39,8.09C2.79,32.65-1.71,56.6.54,80.21h0A105.73,105.73,0,0,0,32.71,96.36,77.7,77.7,0,0,0,39.6,85.25a68.42,68.42,0,0,1-10.85-5.18c.91-.66,1.8-1.34,2.66-2a75.57,75.57,0,0,0,64.32,0c.87.71,1.76,1.39,2.66,2a68.68,68.68,0,0,1-10.87,5.19,77,77,0,0,0,6.89,11.1A105.25,105.25,0,0,0,126.6,80.22h0C129.24,52.84,122.09,29.11,107.7,8.07ZM42.45,65.69C36.18,65.69,31,60,31,53s5-12.74,11.43-12.74S54,46,53.89,53,48.84,65.69,42.45,65.69Zm42.24,0C78.41,65.69,73.25,60,73.25,53s5-12.74,11.44-12.74S96.23,46,96.12,53,91.08,65.69,84.69,65.69Z" style="fill: #5865f2;"></path>
     </svg>`;
     const btn_loadingAnim = `<span class="a-spinner a-spinner-small" style="margin-right: 5px;"></span>`;
-    const btn_checkmark = `<span class='a-button-discord a-button-discord-success' style='background-image: url(https://m.media-amazon.com/images/S/sash/ZNt8quAxIfEMMky.png); background-position: -83px -137px; content: ""; padding: 10px 10px 10px 10px; background-size: 512px 512px; background-repeat: no-repeat; margin-right: 5px; vertical-align: middle;'></span>`;
+    const btn_checkmark = `<span class='a-button-discord a-button-discord-icon a-button-discord-success' style='background-position: -83px -137px;'></span>`;
+    const btn_warning = `<span class='a-button-discord a-button-discord-icon a-button-discord-warning' style='background-position: -83px -117px;'></span>`;
+    const btn_error = `<span class='a-button-discord a-button-discord-icon a-button-discord-error' style='background-position: -451px -421px;'></span>`;
+    const btn_info = `<span class='a-button-discord a-button-discord-icon a-button-discord-info' style='background-position: -257px -353px;'></span>`;
 
-    (GM_getValue("config")) ? GM_getValue("config") : GM_setValue("config", {}); // initialize the list of items that were posted to Discord
+    function verifyToken(token) {
+        return new Promise((resolve, reject) => {
+            GM_xmlhttpRequest({
+                method: "GET",
+                url: `https://api.llamastories.com/brenda/user/${token}`,
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                onload: function(response) {
+                    console.log(response.status, response.responseText);
+                    resolve(response);
+                },
+                onerror: function(error) {
+                    console.error(error);
+                    reject(error);
+                },
+            });
+        });
+    }
+
+    async function askForToken(reason) {
+        return new Promise(async (resolve, reject) => {
+            var userInput = prompt(`Your API token is ${reason}. Please enter a valid token:`);
+
+            if (userInput !== null) {
+                try {
+                    var response = await verifyToken(userInput);
+                    if (response && response.status === 200) {
+                        // Save token after validation
+                        GM_setValue('apiToken', userInput);
+                        resolve(userInput);
+                    } else if (response && response.status === 404) {
+                        alert("API token is invalid!");
+                        reject("Invalid API token");
+                    } else {
+                        alert("API token authorization failed. Please try again later.");
+                        reject("Authorization failed");
+                    }
+                } catch (error) {
+                    console.error("Error verifying API token:", error);
+                    reject(error);
+                }
+            } else {
+                reject("Error: User closed the prompt. A valid API token is required.");
+            }
+        });
+    }
+
+    function returnVariations() {
+        var variations = {};
+
+        document.querySelectorAll(`#vvp-product-details-modal--variations-container .vvp-variation-dropdown`).forEach(function(elem) {
+
+            const type = elem.querySelector('h5').innerText;
+            const names = Array.from(elem.querySelectorAll('.a-dropdown-container select option')).map(function(option) {
+                return option.innerText;
+            });
+            variations[type] = names;
+        });
+        return variations;
+    }
+
+    function variationFormatting(variations) {
+        var str = (Object.keys(variations).length > 1) ? '<:dropdown_options:1117467480860922018> ' : '<:dropdown_options:1117467480860922018> ';
+        for (const type in variations) {
+            const t = (variations[type].length > 1) ? `${type}s` : `${type}`; // plural, if multiple
+            str += `\u200B\n${t}: ${variations[type].join('; ')} | `;
+        }
+
+        return str.slice(0, -2);
+    }
 
     function writeComment(productData) {
-
         var comment = '';
-        comment += (productData.isLimited) ? "<:limited_ltd:1117538207362457611> Limited <:limited_ltd:1117538207362457611>" : '';
-        // More coming soon...
-        return comment;
+        comment += (productData.isLimited) ? "<:limited_ltd:1117538207362457611> Limited" : '';
+        comment += (productData.variations) ? variationFormatting(productData.variations) : '';
 
+        return comment;
     }
 
     function addShareButton() {
@@ -78,24 +171,25 @@ NOTES:
             discordBtn.innerHTML = `${btn_checkmark} Done!`;
             discordBtn.disabled = true;
         } else if (type == 3) { // API: posting too quickly
-            discordBtn.innerHTML = `Sharing too quickly!`;
+            discordBtn.innerHTML = `${btn_warning} Sharing too quickly!`;
             discordBtn.disabled = false;
             discordBtn.style.cursor = 'pointer';
         } else if (type == 4) { // Item was already posted to Discord
-            discordBtn.innerHTML = `Already posted`;
+            discordBtn.innerHTML = `${btn_info} Already posted`;
             discordBtn.disabled = true;
             discordBtn.style.cursor = 'no-drop';
         } else if (type == 5) { // API: invalid token
-            discordBtn.innerHTML = `Invalid token`;
+            discordBtn.innerHTML = `${btn_error} Invalid token`;
             discordBtn.disabled = true;
             discordBtn.style.cursor = 'no-drop';
         } else if (type == 6) { // API: incorrect parameters
-            discordBtn.innerHTML = `Something went wrong`;
+            discordBtn.innerHTML = `${btn_warning} Something went wrong`;
+            discordBtn.disabled = false;
+            discordBtn.style.cursor = 'pointer';
         }
 
     }
 
-    // Function to send the PUT request
     function sendDataToAPI(data) {
 
         const formData = new URLSearchParams({
@@ -109,38 +203,25 @@ NOTES:
         });
 
         updateButton(1);
-        var listOfItems = GM_getValue('config');
 
-        // Send the PUT request
-        GM_xmlhttpRequest({
-            method: "PUT",
-            url: "https://api.llamastories.com/brenda/product",
-            data: formData.toString(),
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-            onload: function(response) {
-                console.log(response.status, response.responseText);
-                if (response.status == 200) { // successfully posted to Discord
-                    listOfItems[data.asin] = {};
-                    listOfItems[data.asin].status = 'Posted';
-                    listOfItems[data.asin].queue = data.queue;
-                    listOfItems[data.asin].date = new Date().getTime();
-                    updateButton(2);
-                } else if (response.status == 401) { // invalid token
-                    updateButton(5);
-                } else if (response.status == 422) { // incorrect parameters; API might have been updated
+        return new Promise((resolve, reject) => {
+            GM_xmlhttpRequest({
+                method: "PUT",
+                url: "https://api.llamastories.com/brenda/product",
+                data: formData.toString(),
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+                onload: function(response) {
+                    console.log(response.status, response.responseText);
+                    resolve(response);
+                },
+                onerror: function(error) {
+                    console.error(error);
                     updateButton(6);
-                } else if (response.status == 429) { // too many requests
-                    updateButton(3);
-                }
-
-                GM_setValue('config', listOfItems);
-            },
-            onerror: function(error) {
-                console.error(error);
-                updateButton(6);
-            },
+                    reject(error);
+                },
+            });
         });
 
     }
@@ -184,6 +265,7 @@ NOTES:
         var target, observer, config;
 
         target = document.querySelector("a#vvp-product-details-modal--product-title");
+
         config = {
             characterData: false,
             attributes: true,
@@ -192,26 +274,54 @@ NOTES:
         };
 
         // Triggers when the Discord button is clicked
-        function buttonClickListener() {
+        async function buttonHandler() {
 
             // Prepping data to be sent to the API
             var productData = {};
-            var modalAsin = document.querySelector("a#vvp-product-details-modal--product-title").href.match(/amazon..+\/dp\/([A-Z0-9]+).*$/)[1];
-            productData.variations = null; // TO-DO
+            var childAsin = document.querySelector("a#vvp-product-details-modal--product-title").href.match(/amazon..+\/dp\/([A-Z0-9]+).*$/)[1];
+            productData.variations = returnVariations();
             productData.isLimited = (document.querySelector('#vvp-product-details-modal--limited-quantity').style.display !== 'none') ? true : false;
             productData.asin = parentAsin;
-            productData.differsFromModal = (parentAsin !== modalAsin) ? true : false; // comparing the asin loaded in the modal to the one on the webpage
+            productData.differentChild = (parentAsin !== childAsin) ? true : false; // comparing the asin loaded in the modal to the one on the webpage
             productData.etv = document.querySelector("#vvp-product-details-modal--tax-value-string")?.innerText.replace("$", "");
             productData.queue = queueType;
             productData.comments = writeComment(productData);
-
             // add in some other things to do...
 
-            sendDataToAPI(productData);
+            const response = await sendDataToAPI(productData);
+
+            var listOfItems = GM_getValue('config');
+
+            if (response) {
+                if (response.status == 200) { // successfully posted to Discord
+                    listOfItems[productData.asin] = {};
+                    listOfItems[productData.asin].status = 'Posted';
+                    listOfItems[productData.asin].queue = productData.queue;
+                    listOfItems[productData.asin].date = new Date().getTime();
+                    GM_setValue('config', listOfItems);
+                    updateButton(2);
+                } else if (response.status == 400 || response.status == 401) { // invalid token
+                    updateButton(5);
+                    // Will prompt the user to enter a valid token
+                    askForToken("missing/invalid").then((value) => {
+                        API_TOKEN = value;
+                        buttonHandler(); // retry the API request
+                    }).catch((error) => {
+                        console.error(error);
+                    });
+                } else if (response.status == 422) { // incorrect parameters (API might have been updated) or posting is paused
+                    updateButton(6);
+                } else if (response.status == 429) { // too many requests
+                    updateButton(3);
+                }
+            }
+
         }
 
         // Mutation observer fires every time the product title in the modal changes
         observer = new MutationObserver(function (mutations) {
+            console.log(mutations);
+
             if (!document.querySelector('.a-button-discord')) {
                 addShareButton();
             }
@@ -219,7 +329,7 @@ NOTES:
             document.querySelector("button.a-button-discord").style.display = 'inline-flex';
 
             // remove pre-existing event listener before creating a new one
-            document.querySelector("button.a-button-discord")?.removeEventListener("click", buttonClickListener);
+            document.querySelector("button.a-button-discord")?.removeEventListener("click", buttonHandler);
 
             // making sure there aren't any errors in the modal
             var hasError = !Array.from(errorMessages).every(function(elem) {
@@ -235,14 +345,18 @@ NOTES:
                 // Product was already posted from the same queue before
                 updateButton(4);
             } else if (!isModalHidden) {
-
+                console.log("Adding event listener to button for asin "+parentAsin);
                 updateButton(0);
-                document.querySelector("button.a-button-discord").addEventListener("click", buttonClickListener);
+                document.querySelector("button.a-button-discord").addEventListener("click", buttonHandler);
             }
 
         });
 
-        observer.observe(target, config);
+        try {
+            observer.observe(target, config);
+        } catch(error) {
+            console.log('No items on the page.');
+        }
 
     });
 
